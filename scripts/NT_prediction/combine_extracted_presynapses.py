aaf86a6dfaee689968b1eb37a909576777e7fb8e
Michael Winding <mwinding@alumni.nd.edu>
2022-11-09 11:31:31 +0000

    fixed bug, cleaned up outputs

--- scripts/NT_prediction/combine_extracted_presynapses.py
+++ scripts/NT_prediction/combine_extracted_presynapses.py
@@ -9,21 +9,21 @@ from joblib import Parallel, delayed
 import glob
 
 path = input("Enter path of HDF5s to concatenate:")
-print("Path is: " + path)
+print("\nPath is: " + path)
 
 pathnames = glob.glob(path + '*.hdf5')
-print(f'Combining HDF5s...')
+print(f'\nCombining HDF5s...')
 print(f'from {pathnames[0]}...')
-print(f'to {pathnames[len(pathnames)]}...')
+print(f'to {pathnames[len(pathnames)-1]}...')
 
 dest_path = input('Enter path of new combined HDF5:')
-print("Save path is: " + dest_path)
+print("\nSave path is: " + dest_path)
 
 # confirm the user wants to continue
 confirm = input('Do you want to continue? [y(es) or no]')
 
 # keep asking if invalid answers are given
-while ((confirm not in ['yes', 'y', 'Yes', 'Y']) | (confirm!='no')):
+while ((confirm not in ['yes', 'y', 'Yes', 'Y']) & (confirm!='no')):
     print('Answer invalid!')
     confirm = input('Do you want to continue? [y(es) or no]')
 
@@ -35,12 +35,14 @@ if(confirm == 'no'):
 t1 = datetime.now()
 
 # combine all data into one HDF5 file
-with h5py.File(dest, 'w') as dest_hdf5:
+with h5py.File(dest_path, 'w') as dest_hdf5:
+
+    dest_hdf5.create_group('all_brain_presynapses')
+
     for i in tqdm(range(len(pathnames))):
 
         source = pathnames[i]
         source_hdf5 = h5py.File(source, 'r')
-        dest_hdf5.create_group('all_brain_presynapses')
 
         for key in source_hdf5['brain_presynaptic_sites'].keys():
             source_hdf5.copy(f'brain_presynaptic_sites/{key}', dest_hdf5['all_brain_presynapses'])
@@ -53,6 +55,6 @@ t2 = datetime.now()
 elapsed = t2-t1
 elapsed = f'{elapsed.total_seconds()//60} min, {elapsed.total_seconds()//1} sec'
 
-print('Finished writing combined HDF5...')
+print('\nFinished writing combined HDF5...')
 print(f'Save location: {dest_path}')
 print(f'Time elapsed: ' + elapsed)
